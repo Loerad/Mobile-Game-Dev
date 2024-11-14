52ba83f2a4d99b6259ecb129dbcb3530f61f106f
rohan <rohan.anakin@gmail.com>
2024-11-15 01:30:01 +1300

    feat: remove incorrect help line

--- Factory/Assets/Scripts/Help.cs
+++ Factory/Assets/Scripts/Help.cs
@@ -13,7 +13,6 @@ public class Help : MonoBehaviour
     "To connect foundries and factories, after placing, tap and drag to start a belt. \nIf you finish the belt on a building that has a input you should see the indicator light up.\n\n"+
     "The first belt you connect will light up the left indicator, no matter which side you connect it from, and will only put numbers into the first slot. This is the same for the second slot\n\n"+
     "You can upgrade foundries by tapping on them again after connecting it's output belt to a factory. Upgrading costs the same as placing foundry.\n\n"+
-    "In delete mode, if you tap a factory with numbers inside that is not currently producing a new number, it will clear those numbers before deleting, requiring you to press again if you want to delete the factory.\n\n"+
     "To delete belts you must delete either one of the connected buildings."+
     "The lowest a number can be is 1.\n\n";
 
